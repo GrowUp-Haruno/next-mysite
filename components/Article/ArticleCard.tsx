3ebfb03c0bd7f1ac43d7f270105cef51b6e30169
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-06-20 13:03:16 +0900

    表示項目の修正

--- components/Article/ArticleCard.tsx
+++ components/Article/ArticleCard.tsx
@@ -4,7 +4,6 @@ import { formatDate } from '../../libs/formatDate';
 import { contentsType } from '../../models/microcms';
 import style from './ArticleCard.module.scss';
 
-
 export const ArticleCard: NextPage<{ contents: contentsType }> = (props) => {
   const createDate = formatDate(new Date(props.contents.createdAt));
   const updateDate = formatDate(new Date(props.contents.updatedAt));
@@ -13,6 +12,8 @@ export const ArticleCard: NextPage<{ contents: contentsType }> = (props) => {
     <article className={style.articleCard}>
       <Image src={props.contents.eyecatch.url} alt="サムネイル１" width={360} height={189} objectFit="contain" />
       <div>
+        <h3>{props.contents.subTitle}</h3>
+        <h2>{props.contents.mainTitle}</h2>
         <p>
           <time dateTime={props.contents.createdAt}>
             <Image src="/icons/filePenSolid.svg" width={12} height={12} alt="作成日時" />
@@ -24,11 +25,14 @@ export const ArticleCard: NextPage<{ contents: contentsType }> = (props) => {
             {updateDate}
           </time>
         </p>
-        <h3>{props.contents.subTitle}</h3>
-        <h2>{props.contents.mainTitle}</h2>
         <div>
-          <span>React</span>
-          <span>Next.js</span>
+          <ul>
+            {props.contents.categories.map((category) => (
+              <li key={category.id}>
+                <span>{category.name}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </article>
